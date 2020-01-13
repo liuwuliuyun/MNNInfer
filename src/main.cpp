161cedd1876e781a32c95ce52d02b9aa64312de6
Yun Liu <liuwuliuyun@pi.com>
2020-01-13 10:21:13 +0800

    update

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "landmark.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    string mnn_path = argv[1];
+    Landmark 
+}
